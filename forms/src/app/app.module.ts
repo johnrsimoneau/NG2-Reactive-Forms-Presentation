a6ce7eb115e7b09f316476777d91f086da6f9121
John Simoneau <jsimoneau@cardinalsolutions.com>
2017-05-21 12:06:02 -0400

    Added form component & bootstrap

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
