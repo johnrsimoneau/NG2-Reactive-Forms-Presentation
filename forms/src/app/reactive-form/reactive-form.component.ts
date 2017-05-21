0ed6908682d4b40ae8a5f0ea92853a0165718c06
John Simoneau <jsimoneau@cardinalsolutions.com>
2017-05-21 12:31:37 -0400

    Added reactive forms modeule

--- forms/src/app/reactive-form/reactive-form.component.ts
+++ forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
